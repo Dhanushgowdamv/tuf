d33ad71044c042873203ca53f886eecdc3c7fafe
Dhanush Gowda mv <dhanushvenu199@gmail.com>
2024-12-18 19:25:02 +0530

    pattern 7 done

--- patterns/pattern1.java
+++ patterns/pattern1.java
@@ -11,7 +11,6 @@ public class pattern1 {
             System.out.println();
         }
     }
-
     public static void pattern2(int n) {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < i; j++) {
@@ -20,7 +19,6 @@ public class pattern1 {
             System.out.println();
         }
     }
-
     public static void pattern3(int n) {
         for (int i = 1; i <=n; i++) {
             for (int j = 1; j <= i; j++) {
@@ -29,6 +27,56 @@ public class pattern1 {
             System.out.println();
         }
     }
+    public static void pattern4(int n) {
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern5(int n) {
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <n-i+1; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern6(int n) {
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <n-i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern7(int n){
+        //outer loops
+        for(int i=0;i<n;i++){
+            //prit the space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            //print the stars
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        
+    }
+
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
         int n = 5;
@@ -37,6 +85,7 @@ public class pattern1 {
         pattern1 obj = new pattern1();
         obj.pattern(n);
         obj.pattern2(n);
-        obj.pattern3(n);
+        obj.pattern7(n);
     }
 }
+   
