c05be9a70e641347b9bd6d706b06be68a5f85d9d
Dhanush Gowda mv <dhanushvenu199@gmail.com>
2024-12-21 23:11:21 +0530

    binary right angle is over

--- patterns/pattern1.java
+++ patterns/pattern1.java
@@ -89,8 +89,7 @@ public class pattern1 {
         }
         
     }
-
-public static void pattern9(int n){
+    public static void pattern9(int n){
     for(int i=0;i<n;i++){
         //prit the space
         for(int j=0;j<n-i-1;j++){
@@ -123,8 +122,32 @@ public static void pattern9(int n){
     }
 
 }
-
-
+    public static void pattern10(int n){
+        for(int i=1 ;i< n;i++){
+            for(int j=1;j< i ;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for( int i= n-1;i >=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern11(int n){
+        int start = 1;
+        for(int i=0;i<n;i++){
+            if(i %2 == 0) start = 1;
+            else start=0;
+            for(int j =0 ;j<=i;j++){
+                System.out.print(start);
+                start = 1- start;
+            }
+            System.out.println();
+        }
+    }
 
 
 
@@ -135,9 +158,8 @@ public static void pattern9(int n){
 
         // Create an instance of Pattern1 and call the pattern method
         pattern1 obj = new pattern1();
-        obj.pattern(n);
-        obj.pattern2(n);
-        obj.pattern9(n);
+        
+        obj.pattern11(n);
     }
 }
    
