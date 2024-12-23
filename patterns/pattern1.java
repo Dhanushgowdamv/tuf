735b5071f2832a060ad1cc72bfa2d22022bde6ee
Dhanush Gowda mv <dhanushvenu199@gmail.com>
2024-12-23 16:18:06 +0530

    next will be start with the letter printing

--- patterns/pattern1.java
+++ patterns/pattern1.java
@@ -90,38 +90,35 @@ public class pattern1 {
         
     }
     public static void pattern9(int n){
-    for(int i=0;i<n;i++){
-        //prit the space
-        for(int j=0;j<n-i-1;j++){
-            System.out.print(" ");
+        for(int i=0;i<n;i++){
+            //prit the space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            //print the stars
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
         }
-        //print the stars
-        for(int j=0;j<2*i+1;j++){
-            System.out.print("*");
+        for(int i=0;i<n;i++){
+            //prit the space
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            //print the stars
+            for(int j=0;j<(2*n-(2*i +1));j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
         }
-        for(int j=0;j<n-i-1;j++){
-            System.out.print(" ");
-        }
-        System.out.println();
     }
-
-
-    for(int i=0;i<n;i++){
-        //prit the space
-        for(int j=0;j<i;j++){
-            System.out.print(" ");
-        }
-        //print the stars
-        for(int j=0;j<(2*n-(2*i +1));j++){
-            System.out.print("*");
-        }
-        for(int j=0;j<i;j++){
-            System.out.print(" ");
-        }
-        System.out.println();
-    }
-
-}
     public static void pattern10(int n){
         for(int i=1 ;i< n;i++){
             for(int j=1;j< i ;j++){
@@ -148,7 +145,39 @@ public class pattern1 {
             System.out.println();
         }
     }
+    public static void pattern12(int n){
 
+        int space = 2*(n-1);
+        for(int i=1;i<=n;i++){
+            
+            //number
+            for(int j = 1;j<=i;j++){
+                System.out.print(j);
+            }
+            //space = 2*(n-1) for reducing by two
+            for(int j=1;j<=space;j++){
+                System.out.print(" ");
+            }
+            //number
+            for(int j = i;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+            space -=2;
+        }
+
+    }
+    public static void pattern13(int n){
+        int num = 1;
+        for(int i=0;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num);
+                num= num+1;
+            }
+            System.out.println();
+        }
+    }
+        
 
 
 
@@ -159,7 +188,7 @@ public class pattern1 {
         // Create an instance of Pattern1 and call the pattern method
         pattern1 obj = new pattern1();
         
-        obj.pattern11(n);
+        obj.pattern13(n);
     }
 }
    
