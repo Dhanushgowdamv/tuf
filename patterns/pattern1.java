4f43aab4ae99aca9682ef3339eadaab836e6427c
Dhanush Gowda mv <dhanushvenu199@gmail.com>
2025-01-06 21:52:56 +0530

    start with basic of the math

--- patterns/pattern1.java
+++ patterns/pattern1.java
@@ -177,18 +177,105 @@ public class pattern1 {
             System.out.println();
         }
     }
-        
+    public static void pattern14(int n){
+        for(int i=0;i<=n;i++){
+            for(char ch='A';ch<='A'+i;ch++){
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern15(int n){
 
+        for(int i=0;i<n;i++){
+            for(char j='A';j<='A'+(n-i-1);j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern16(int n){
+        for(int i=0;i<n;i++){
+            char ch =(char) ('A' + i);
+            for(int j=0;j<=i;j++){
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern17(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i-1;j++){
+                System.out.print("* ");
+            }
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern18(int n){
+        for(int i=0;i<n;i++){
+            for(char j=(char) ('E'-i);j<='E';j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern19(int N){
+        for (int i = 0; i < N; i++) {
+            // Print left stars
+            for (int j = 0; j < N - i; j++) {
+                System.out.print("* ");
+            }
 
+            // Print spaces
+            for (int j = 0; j < 2 * i; j++) {
+                System.out.print("  ");
+            }
+
+            // Print right stars
+            for (int j = 0; j < N - i; j++) {
+                System.out.print("* ");
+            }
+
+            // Move to the next row
+            System.out.println();
+        }
+
+        // Generate the bottom half of the pattern
+        for (int i = N - 1; i >= 0; i--) {
+            // Print left stars
+            for (int j = 0; j < N - i; j++) {
+                System.out.print("* ");
+            }
+
+            // Print spaces
+            for (int j = 0; j < 2 * i; j++) {
+                System.out.print("  ");
+            }
+
+            // Print right stars
+            for (int j = 0; j < N - i; j++) {
+                System.out.print("* ");
+            }
+
+            // Move to the next row
+            System.out.println();
+        }
+    }
 
 
     public static void main(String[] args) {
-        int n = 5;
+        int N = 5;
 
         // Create an instance of Pattern1 and call the pattern method
         pattern1 obj = new pattern1();
         
-        obj.pattern13(n);
+        obj.pattern19(N);
     }
 }
    
