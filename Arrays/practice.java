8ac891c16b87795cc0042299843f3bbfc141fa41
Dhanush Gowda mv <dhanushvenu199@gmail.com>
2025-02-10 08:36:48 +0530

    second large and second smallest

--- Arrays/practice.java
+++ Arrays/practice.java
@@ -1,4 +1,4 @@
-public class Practice {
+public class practice {
 
     public static int large(int n, int[] arr) {
         int largest = arr[0];  // Assume the first element is the largest
@@ -11,12 +11,46 @@ public class Practice {
         return largest;  // Return the largest number
     }
 
-    public static void seclarge(int [] arr,int n){
-        
+
+    // time comlpicaty is O(n)
+    public static int secondlargest(int [] a,int n){
+        int largest = a[0];
+        int slargest = -1;
+         for(int i=0;i<n;i++){
+            if(a[i] > largest){
+                slargest = largest;
+                largest = a[i];
+
+            }else if(a[i]< largest && a[i]>slargest ){
+                slargest = a[i]; 
+            }
+         }
+         return slargest;
+
+    }
+
+    public static int secondsmallest(int []a ,int n){
+          int smallest = a[0];
+          int ssmallest = Integer.MAX_VALUE;
+          for(int i=1;i<n;i++){
+            if(a[i] <smallest){
+                ssmallest = smallest;
+                smallest = a[i];
+            }else if(a[i] != smallest && a[i] < ssmallest){
+                ssmallest = a[i];
+            }
+          }
+          return ssmallest;
     }
     public static void main(String[] args) {
         int[] numbers = {10, 25, 35, 42, 5, 19};  // Sample array
         int largestNumber = large(numbers.length, numbers);  // Calling the method
         System.out.println("Largest number is: " + largestNumber);  // Output the result
+        int[] a= {1,2,4,7,2,9,3};
+        int n = a.length;
+        int slargest = secondlargest(a,n);
+        int ssmallest= secondsmallest(a,n);
+       System.out.println("second Largest "+slargest);
+        System.out.println("second smallest"+ssmallest);
     }
 }
