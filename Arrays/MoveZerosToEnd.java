5b23b70ea5cdcd0c47523932c632ce040283ca60
Dhanushgowdamv <dhaunshvenu199@gmail.com>
2025-02-15 11:03:37 +0530

    better optmail

--- Arrays/MoveZerosToEnd.java
+++ Arrays/MoveZerosToEnd.java
@@ -23,7 +23,35 @@ public class MoveZerosToEnd {
 
        
     }
+
+    // better way to solve the question
+    public static void moveZeroBetter(List<Integer> arr){
+        int count =0 ;
+        for(int i=0;i<n;i++){
+            if(arr.get(i) !=0){
+                arr.set(count++ , arr.get(i));
+            }
+
+
+        }
+        while(count < arr.size()){
+            arr.set(count++ ,0);
+        }
+    }
    
+    public static void moveZeroOptmail(List<Integer> arr){
+        int j=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr.get(i) != 0){
+                if(i!=j){
+                    int temp = arr.get(i);
+                    arr.set(i,arr.get(j));
+                    arr.set(j,temp);
+                }
+                j++;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<>(Arrays.asList(0,1,0,3,12,6,0,4,0,88));
