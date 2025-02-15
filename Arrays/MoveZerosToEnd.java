7d07d103ee096881f64425e96fe68d3922cd386e
Dhanushgowdamv <dhaunshvenu199@gmail.com>
2025-02-15 10:53:55 +0530

    brute of the move zers

--- Arrays/MoveZerosToEnd.java
+++ Arrays/MoveZerosToEnd.java
@@ -2,52 +2,32 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MoveZerosToEnd {
-    public static void moveZeros(int[] arr) {
-        int index = 0; // Position for non-zero elements
+    // move the zeroes to the end of the arr in brute way
+    public static void moveZerosBrute(LIst<Integer> arr) {
+        List<Integer> temp = new ArrayList<>();
 
-        // Move non-zero elements forward
-        for (int num : arr) {
-            if (num != 0) {
-                arr[index++] = num;
+        for(int num : arr){
+            if(num !=0){
+                temp.add(num);
             }
         }
 
-        // Fill remaining spaces with zeros
-        while (index < arr.length) {
-            arr[index++] = 0;
+        int i=0;
+        for(;i < temp.size();i++){
+            arr.set(i,temp.get(i));
         }
-    }
 
-    public static int search(int[] arr, int target) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
-                return i; // Return the index where the target is found
-            }
+        for(;i<arr.size();i++){
+            arr.set(i,0);
         }
-        return -1; // Return -1 if the element is not found
+
+       
     }
+   
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // Scanner object created
-
-        int[] arr = {0, 1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9};
-        System.out.println("Before: " + Arrays.toString(arr));
-
-        moveZeros(arr);
-
-        System.out.println("After: " + Arrays.toString(arr));
-
-        System.out.print("Enter the number to search: ");
-        int target = scanner.nextInt();
-
-        int result = search(arr, target);
-
-        if (result != -1) {
-            System.out.println("Element found at index: " + result);
-        } else {
-            System.out.println("Element not found in the array.");
-        }
-
-        scanner.close(); // Close the scanner
-    }
+        List<Integer> arr = new ArrayList<>(Arrays.asList(0,1,0,3,12,6,0,4,0,88));
+        moveZerosBrute(arr);
+        System.out.println(arr);
+}
 }
